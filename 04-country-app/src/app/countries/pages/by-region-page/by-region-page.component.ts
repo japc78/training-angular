762294fcc9c2b11c059ba2bbde57e2617b7ec279
jpavon <jpavon@prodevelop.es>
2026-02-05 17:18:43 +0100

    feat: :sparkles: Implement region selection and search functionality in ByRegionPage

--- 04-country-app/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ 04-country-app/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'Americas' | 'Asia' | 'Europe' | 'Oceania';
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,15 +12,21 @@ import { Country } from '../../interfaces/country';
 })
 export class ByRegionPageComponent {
 
+
   public countries : Country[] = [];
+  public regions : Region[] = ['Africa', 'Americas', 'Asia', 'Europe', 'Oceania'];
 
   constructor(private countriesService: CountriesService) { }
 
-    onSearch(value: string) :void{
+  onSearch(value: Region) :void{
     console.log('Search by Region value:', value);
     this.countriesService.searchRegion(value).subscribe(countries => {
       this.countries = countries;
     });
   }
 
+  onRegionChange($event: Event) {
+    const region = ($event.target as HTMLInputElement).value as Region;
+    this.onSearch(region);
+  }
 }
