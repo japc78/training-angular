642fbebb777d7c4af8db076f69f4ae188bd6cd26
japc78 <japc.dev@gmail.com>
2026-02-10 22:48:48 +0100

    fix: correct path for items module and fix typo in uncommon route

--- 05-pipes-app/src/app/app-routing.module.ts
+++ 05-pipes-app/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'items', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) }
+  { path: '', loadChildren: () => import('./items/items.module').then(m => m.ItemsModule) }
 ];
 
 @NgModule({
--- 05-pipes-app/src/app/items/items-routing.module.ts
+++ 05-pipes-app/src/app/items/items-routing.module.ts
@@ -14,7 +14,7 @@ const routes: Routes = [
     component: NumbersPageComponent
   },
   {
-    path: 'uncoommon',
+    path: 'uncommon',
     component: UncommonPageComponent
   },
   {
